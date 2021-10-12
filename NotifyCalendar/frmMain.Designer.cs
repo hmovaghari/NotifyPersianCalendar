64f424b747578d8b6fdd007625bd55ecb05fb540
Hamed Movaghari <hmovaghari@gmail.com>
2021-10-12 21:53:06 +0330

    Change name of UI items

--- NotifyCalendar/frmMain.Designer.cs
+++ NotifyCalendar/frmMain.Designer.cs
@@ -34,10 +34,10 @@ namespace NotifyCalendar
             this.notify = new System.Windows.Forms.NotifyIcon(this.components);
             this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.btnEvents = new System.Windows.Forms.ToolStripMenuItem();
-            this.ایجادرویدادجدیدToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.تغییرصفحهنمایشToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAddEvent = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnBackgroundChenger = new System.Windows.Forms.ToolStripMenuItem();
             this.btnOpenAlbum = new System.Windows.Forms.ToolStripMenuItem();
-            this.تبدیلتاریخToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDateConvert = new System.Windows.Forms.ToolStripMenuItem();
             this.btnSettings = new System.Windows.Forms.ToolStripMenuItem();
             this.btnAbout = new System.Windows.Forms.ToolStripMenuItem();
             this.btnExit = new System.Windows.Forms.ToolStripMenuItem();
@@ -58,10 +58,10 @@ namespace NotifyCalendar
             this.menu.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnEvents,
-            this.ایجادرویدادجدیدToolStripMenuItem,
-            this.تغییرصفحهنمایشToolStripMenuItem,
+            this.btnAddEvent,
+            this.btnBackgroundChenger,
             this.btnOpenAlbum,
-            this.تبدیلتاریخToolStripMenuItem,
+            this.btnDateConvert,
             this.btnSettings,
             this.btnAbout,
             this.btnExit});
@@ -76,19 +76,19 @@ namespace NotifyCalendar
             this.btnEvents.Text = "رویداد امروز";
             this.btnEvents.Visible = false;
             // 
-            // ایجادرویدادجدیدToolStripMenuItem
+            // btnAddEvent
             // 
-            this.ایجادرویدادجدیدToolStripMenuItem.Name = "ایجادرویدادجدیدToolStripMenuItem";
-            this.ایجادرویدادجدیدToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            this.ایجادرویدادجدیدToolStripMenuItem.Text = "ایجاد رویداد جدید";
-            this.ایجادرویدادجدیدToolStripMenuItem.Visible = false;
+            this.btnAddEvent.Name = "btnAddEvent";
+            this.btnAddEvent.Size = new System.Drawing.Size(177, 22);
+            this.btnAddEvent.Text = "ایجاد رویداد جدید";
+            this.btnAddEvent.Visible = false;
             // 
-            // تغییرصفحهنمایشToolStripMenuItem
+            // btnBackgroundChenger
             // 
-            this.تغییرصفحهنمایشToolStripMenuItem.Name = "تغییرصفحهنمایشToolStripMenuItem";
-            this.تغییرصفحهنمایشToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            this.تغییرصفحهنمایشToolStripMenuItem.Text = "تغییر پس زمینه";
-            this.تغییرصفحهنمایشToolStripMenuItem.Click += new System.EventHandler(this.BackgroundChenger);
+            this.btnBackgroundChenger.Name = "btnBackgroundChenger";
+            this.btnBackgroundChenger.Size = new System.Drawing.Size(177, 22);
+            this.btnBackgroundChenger.Text = "تغییر پس زمینه";
+            this.btnBackgroundChenger.Click += new System.EventHandler(this.BackgroundChenger);
             // 
             // btnOpenAlbum
             // 
@@ -97,12 +97,12 @@ namespace NotifyCalendar
             this.btnOpenAlbum.Text = "آلبوم تصاویر پس‌زمینه";
             this.btnOpenAlbum.Click += new System.EventHandler(this.btnOpenAlbum_Click);
             // 
-            // تبدیلتاریخToolStripMenuItem
+            // btnDateConvert
             // 
-            this.تبدیلتاریخToolStripMenuItem.Name = "تبدیلتاریخToolStripMenuItem";
-            this.تبدیلتاریخToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
-            this.تبدیلتاریخToolStripMenuItem.Text = "تبدیل تاریخ";
-            this.تبدیلتاریخToolStripMenuItem.Visible = false;
+            this.btnDateConvert.Name = "btnDateConvert";
+            this.btnDateConvert.Size = new System.Drawing.Size(177, 22);
+            this.btnDateConvert.Text = "تبدیل تاریخ";
+            this.btnDateConvert.Visible = false;
             // 
             // btnSettings
             // 
@@ -153,9 +153,9 @@ namespace NotifyCalendar
         private System.Windows.Forms.ToolStripMenuItem btnAbout;
         private System.Windows.Forms.ToolStripMenuItem btnExit;
         private System.Windows.Forms.Timer timer;
-        private System.Windows.Forms.ToolStripMenuItem تغییرصفحهنمایشToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem ایجادرویدادجدیدToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem تبدیلتاریخToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnBackgroundChenger;
+        private System.Windows.Forms.ToolStripMenuItem btnAddEvent;
+        private System.Windows.Forms.ToolStripMenuItem btnDateConvert;
         private System.Windows.Forms.ToolStripMenuItem btnOpenAlbum;
     }
 }
