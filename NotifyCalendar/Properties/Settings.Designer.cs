ad8abe17c01fbd1301cefa7691b7f40762bdab63
H Movaghari <hmovaghari@gmail.com>
2023-03-10 09:56:23 +0330

    default settings

--- NotifyCalendar/Properties/Settings.Designer.cs
+++ NotifyCalendar/Properties/Settings.Designer.cs
@@ -168,7 +168,7 @@ namespace NotifyCalendar.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("3")]
         public byte BackgroundLocation {
             get {
                 return ((byte)(this["BackgroundLocation"]));
@@ -204,7 +204,7 @@ namespace NotifyCalendar.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("3")]
         public int BackgroundStyle {
             get {
                 return ((int)(this["BackgroundStyle"]));
